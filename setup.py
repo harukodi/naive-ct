f8b2cfbc4ea7350fe3a49c7c6d8803e609095aae
xia1997x <nikolamihov2@gmail.com>
2025-06-23 13:43:34 +0000

    small fixes

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from string import Template
 
-def setup_caddyfile(username, password, domain):
+def setup_caddyfile(username, password, domain_name):
     with open("./templates/Caddyfile", "r") as caddy_template:
         template = caddy_template.read()
         caddyfile = Template(template)
         caddy_config = {
             "USERNAME": username,
             "PASSWORD": password,
-            "DOMAIN_NAME": domain
+            "DOMAIN_NAME": domain_name
         }
         caddyfile_template = caddyfile.substitute(caddy_config)
     with open("./caddy/Caddyfile", "w") as output_file:
